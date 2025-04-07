e894e999b2d5691d7c5dce4db8f602fc1e667e08
= <=>
2025-04-07 18:37:26 -0300

    Adicionado swagger na api

---
+++ LojaVirtual/LojaVirtual.Api/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace LojaVirtual.Api.Extensions
+{
+    public static class WebApplicationBuilderExtensions
+    {
+        public static void AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+            builder.Services.AddOpenApi();
+        }
+    }
+}
---
+++ LojaVirtual/LojaVirtual.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace LojaVirtual.Api.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static void ConfigurePipeline(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI(options =>
+                {
+                    options.SwaggerEndpoint("/swagger/v1/swagger.json", "Loja Virtual API V1");
+                });
+            }
+
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+        }
+    }
+}
