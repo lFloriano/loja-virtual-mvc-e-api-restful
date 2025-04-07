7ef4e8e04afbe2fe81053a0bcd7261a670773629
= <=>
2025-04-07 19:33:01 -0300

    Adicionados formatadores de resposta no CategoriaController

---
+++ LojaVirtual/LojaVirtual.Api/Controllers/CategoriasController.cs
@@ -0,0 +1,79 @@
+﻿using LojaVirtual.Core.Application.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LojaVirtual.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoriasController : ControllerBase
+    {
+        List<Categoria> categorias = new List<Categoria>();
+
+        [HttpGet]
+        [ProducesResponseType(typeof(IEnumerable<Categoria>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public IActionResult Get()
+        {
+            if (categorias == null || !categorias.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(categorias);
+        }
+
+        [HttpGet("{id:int}")]
+        [ProducesResponseType(typeof(Categoria), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Get(int id)
+        {
+            var categoria = categorias.FirstOrDefault(x => x.Id == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(categoria);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(typeof(Categoria), StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult Post([FromBody] Categoria categoria)
+        {
+            //TODO: referencia: aula "action results e Http status codes" aos 05:00 minutos
+            return CreatedAtAction(nameof(Get), new { id = 1 }, categoria);
+        }
+
+        [HttpPut("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Put(int id, [FromBody] Categoria categoria)
+        {
+            var categoriaOriginal = categorias.FirstOrDefault(x => x.Id == id);
+
+            if (categoriaOriginal == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id:int}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public IActionResult Delete(int id)
+        {
+            var categoria = categorias.FirstOrDefault(x => x.Id == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+    }
+}
---
+++ LojaVirtual/LojaVirtual.Core/Application/Models/Categoria.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LojaVirtual.Core.Application.Models
+{
+    public class Categoria
+    {
+        public int Id { get; set; }
+    }
+
+    public class Produto
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public DateTime Criacao{ get; set; }
+    }
+}
